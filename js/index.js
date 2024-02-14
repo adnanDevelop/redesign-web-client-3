bb9e7dab3d7a29f66e2e65c30b1886a9980074fc
Adnan Tariq <adnan6official@gmail.com>
2024-02-14 09:57:19 +0500

    working on review section

--- js/index.js
+++ js/index.js
@@ -60,3 +60,32 @@ const scrollAnimation = () => {
 
 // Add event listener to update the gradient on scroll
 window.addEventListener("scroll", scrollAnimation);
+
+
+// SWIPER JS 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
